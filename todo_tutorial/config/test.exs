1521233948f3e2ebf583314aed71598dcd550727
kuunamaka <mauisahashi@gmail.com>
2020-09-18 00:09:02 +0900

    changed to localhost:4000

--- todo_tutorial/config/test.exs
+++ todo_tutorial/config/test.exs
@@ -11,7 +11,7 @@ config :todo_tutorial, TodoTutorial.Repo,
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :todo_tutorial, TodoTutorialWeb.Endpoint,
-  http: [port: 4002],
+  http: [port: 4000],
   server: false
 
 # Print only warnings and errors during test
